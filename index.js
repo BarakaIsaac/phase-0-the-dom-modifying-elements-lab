fcde53974e2547eb37710c6eca853a157791e913
BarakaIsaac <isaac.mwandau2@gmail.com>
2022-12-12 14:05:29 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.getElementById("main").remove();
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = ("Baraka is the champion");
+document.body.appendChild(newHeader);
